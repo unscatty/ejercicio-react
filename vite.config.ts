b5137c45b08fb140c8262eb2cc86292648b35408
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-04-26 01:29:43 -0600

    Agrega action para despliegue en Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import UnoCSS from 'unocss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ command, mode }) => {
+  let base = '/'
+
+  if (command === 'build' && mode === 'gh-pages') {
+    const env = loadEnv(mode, process.cwd())
+    base = env.VITE_REPO_NAME
+
+    console.log(`Base url is ${base}`)
+  }
+  
+  return {
+    base,
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
-    UnoCSS(),
-    react(),
-  ],
+    plugins: [
+      UnoCSS(),
+      react(),
+    ],
+  }
 })
